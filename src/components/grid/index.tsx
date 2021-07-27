35ca608e628443435b483ec667ebcf2267652e45
Acalltoredemption <acalltoredemption@gmail.com>
2021-07-27 11:55:56 -0700

    implementing basic functionality of grid component

---
+++ src/components/grid/index.tsx
@@ -0,0 +1,18 @@
+import React, {FC} from "react";
+
+const Grid: FC = () => {
+    // Other on the way
+
+    return (
+        <div data-cy="grid-container">
+            {[...Array(9)].map((_, rowIndex) =>(
+            <div data-cy="grid-row-container" key={rowIndex}>
+                {[...Array(9)].map((_, colIndex) => (
+                    <div data-cy="block" key={colIndex}></div>
+                ))}
+            </div>))}
+        </div>
+    )
+}
+
+export default Grid;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {GlobalStyles} from './styles';
-
+import {GlobalStyles, theme} from './styles';
+import {ThemeProvider} from 'styled-components'
+import {Content, Title, Card, Grid} from './components';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <>
+  <ThemeProvider theme={theme}>
   <GlobalStyles />
-    <div>Hello World</div>
-  </>,
+  <Content data-cy="content">
+    <Title data-cy="title">Sudoku</Title>
+    <Card data-cy="card">
+      <Grid/>
+    </Card>
+  </Content>
+  </ThemeProvider>,
   document.getElementById('root')
 );
 

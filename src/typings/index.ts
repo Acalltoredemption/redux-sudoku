0c0f1d73c31bf83eb6bef6a1ee0c8f5599b96467
Acalltoredemption <acalltoredemption@gmail.com>
2021-08-09 16:30:16 -0700

    setting up algorithm that will fill soduku grid

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -1,4 +1,4 @@
-export type GRID = [ROW, ROW, ROW, ROW, ROW, ROW, ROW, ROW, ROW][]
+export type GRID = [ROW, ROW, ROW, ROW, ROW, ROW, ROW, ROW, ROW]
 
 export type NUMBERS = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 
--- src/utils/fill-grid/index.ts
+++ src/utils/fill-grid/index.ts
@@ -1,5 +1,5 @@
-import {GRID} from '../../../src/typings';
-
+import {GRID, NUMBERS} from '../../../src/typings';
+import { shuffle } from 'utils';
 const gridExample = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -12,8 +12,27 @@ const gridExample = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
 
-function fillGrid(grid: GRID){
+const numbers: NUMBERS[] = [1,2,3,4,5,6,7,8,9]
 
+//backtracking/recursive function to check all the possible combinations of numbers until a solution is found
+
+function fillGrid(grid: GRID){
+    let row = 0;
+    let col = 0;
+
+    for (let i = 0; i <81; i++){
+        row = Math.floor(i/9)
+        col = i % 9
+
+        if (grid[row][col] === 0) {
+            shuffle(numbers)
+            //Do stuff
+            //recursive things
+            break;
+        }
+    }
+
+    grid[row][col] = 0;
 }
 
 export default fillGrid;

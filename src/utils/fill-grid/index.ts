81c6bd75b989578958be8e3cd8f53141e74d526b
Acalltoredemption <acalltoredemption@gmail.com>
2021-08-09 17:15:54 -0700

    starting fillGrid logic

--- src/utils/fill-grid/index.ts
+++ src/utils/fill-grid/index.ts
@@ -26,8 +26,32 @@ function fillGrid(grid: GRID){
 
         if (grid[row][col] === 0) {
             shuffle(numbers)
-            //Do stuff
-            //recursive things
+            
+            for(let value of numbers){
+                //is it not in the grid row?
+                if(!grid[row].includes(value)){
+                        //is it not in the grid col?
+                    let isNotInCol = true;
+                    for (let i = 0; i < 9; i++){
+                        if(value === grid[i][col]) isNotInCol = false;
+                    }
+                    if(isNotInCol){
+                        //is it not in the grid square?
+                    }
+                }
+                
+
+                
+                
+                //..... if this is the case
+
+                grid[row][col] = value
+                //check grid if it is full, if yes, stop and return true
+
+                // otherwise run fillgrid(grid) again
+
+            }
+
             break;
         }
     }

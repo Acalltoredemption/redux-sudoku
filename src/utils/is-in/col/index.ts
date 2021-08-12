abe394fc85b9035213e1beead83ecc4c7aa0de83
Acalltoredemption <acalltoredemption@gmail.com>
2021-08-11 18:16:42 -0700

    added testing for isInCol and isInRow

--- src/utils/is-in/col/index.ts
+++ src/utils/is-in/col/index.ts
@@ -7,6 +7,9 @@ interface IInput {
 }
 
 function isInCol({ col, grid, value}: IInput): boolean {
-
+    for (let i = 0; i < 9; i++) if (value === grid[i][col]) return true
     return false
+   
 }
+
+export default isInCol

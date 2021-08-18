09b7ab7941d2976eb807429018026fbf01a535d9
Acalltoredemption <acalltoredemption@gmail.com>
2021-08-18 16:08:01 -0700

    initializing redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {GlobalStyles, theme} from './styles';
-import {ThemeProvider} from 'styled-components'
-import {Content, Title, Card, Grid} from './components';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { GlobalStyles, theme } from './styles'
+import { ThemeProvider } from 'styled-components'
+import { Content, Title, Card, Grid } from './components'
+import reportWebVitals from './reportWebVitals'
+import { Provider } from 'react-redux'
+import { configureStore } from 'core'
+
+const store = configureStore()
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-  <GlobalStyles />
-  <Content data-cy="content">
-    <Title data-cy="title">Sudoku</Title>
-    <Card data-cy="card">
-      <Grid/>
-    </Card>
-  </Content>
+    <GlobalStyles />
+    <Provider store={store}>
+      <Content data-cy="content">
+        <Title data-cy="title">Sudoku</Title>
+        <Card data-cy="card">
+          <Grid />
+        </Card>
+      </Content>
+    </Provider>
   </ThemeProvider>,
   document.getElementById('root')
-);
+)
 
-
-reportWebVitals();
+reportWebVitals()

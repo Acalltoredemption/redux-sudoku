dad08b3e6c2e722360b50acd457262fc6cc5e296
Acalltoredemption <acalltoredemption@gmail.com>
2021-08-24 09:12:35 -0700

    new game functionality

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { GlobalStyles, theme } from './styles'
 import { ThemeProvider } from 'styled-components'
-import { Content, Title, Card, Grid, Numbers } from './components'
+import { Content, Title, Card, NewButton, Grid, Numbers } from './components'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 import { configureStore } from 'core'
@@ -16,6 +16,7 @@ ReactDOM.render(
       <Content data-cy="content">
         <Title data-cy="title">Sudoku</Title>
         <Card data-cy="card">
+          <NewButton />
           <Grid />
           <Numbers />
         </Card>
